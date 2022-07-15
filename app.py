9247b229db03a7a54acec4254068075b84284e5b
陳維勛 <wxchen@gmail.com>
2022-07-15 23:59:41 +0800

    sticker

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def handle_message(event):
         r = 'hi'
     elif msg == '你吃飯了嗎':
         r = '還沒'
-    elif: msg == '你是誰':
+    elif msg == '你是誰':
         r = '我是機器人'
     line_bot_api.reply_message(
         event.reply_token,
